d6b0de41274c4bbf767e1fcc1912c958c87aefdf
kinanaw <131298465+kinanaw@users.noreply.github.com>
2026-03-06 00:02:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 )
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("logo.png", width=200)
+    st.image("logo.png", width=600)
 # ─── Custom CSS ─────────────────────────────────────────────────────────────
 st.markdown("""
 <style>
